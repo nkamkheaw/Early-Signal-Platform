acef0b9d98f5cd8f996e4c877527621570feb260
Nakarin Kamkheaw <nkamkheaw@linkedin.com>
2020-03-02 15:59:49 -0800

    add check suite and check run class

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from bot_config import API_BASE_URL, validate_env_variables
-# from gh_oauth_token import get_token, store_token
-from gh_utils import make_github_rest_api_call
-from webhook_handlers import add_pr_comment
+from gh_oauth_token import get_token, store_token
+from webhook_handlers import check_suite_request_handler
 
 import json
 import logging
@@ -19,14 +18,15 @@ log = logging.getLogger(__name__)
 # Create the Flask App.
 app = Flask(__name__)
 
-
 """
 STATIC PAGES
 ==============
 These pages basically just show static HTML
 
 """
-@app.route('/')
+
+
+@app.route("/")
 def welcome():
     """Welcome page"""
     return render_template("index.html", readme_html=markdown2.markdown_path("./README.md"))
@@ -38,25 +38,26 @@ AUTH ROUTES
 
 Dynamic routes that are needed to facilitate the authentication flow
 
-We will let you know when it's appropriate to un-comment this
+We will let you know when it"s appropriate to un-comment this
 """
 
-# @app.route("/authenticate/<app_id>", methods=["GET"])
-# def authenticate(app_id):
-#     """Incoming Installation Request. Accept and get a new token."""
-#     try:
-#         app_id = str(app_id)
-#         installation_id = request.args.get('installation_id')
-#         store_token(get_token(app_id, installation_id))
 
-#     except Exception:
-#         log.error("Unable to get and store token.")
-#         traceback.print_exc(file=sys.stderr)
+@app.route("/authenticate/<app_id>", methods=["GET"])
+def authenticate(app_id):
+    """Incoming Installation Request. Accept and get a new token."""
+    try:
+        app_id = str(app_id)
+        installation_id = request.args.get("installation_id")
+        store_token(get_token(app_id, installation_id))
 
-#     return redirect("https://www.github.com", code=302)
+    except Exception:
+        log.error("Unable to get and store token.")
+        traceback.print_exc(file=sys.stderr)
+
+    return redirect("https://www.github.com", code=302)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def process_message():
     """
     WEBHOOK RECEIVER
@@ -66,30 +67,31 @@ def process_message():
     to
         http://localhost:5000/webhook
 
-    If you don't see expected payloads arrive here, please check the following
+    If you don"t see expected payloads arrive here, please check the following
 
     - Is your github repo configured to deliver webhooks to a https://smee.io URL?
     - Is your github repo configured to deliver webhook payloads for the right EVENTS?
     - Is your webhook forwarding tool (i.e., pysmee or smee-client) running?
-    - Is github SENDING webhooks to the same https://smee.io URL you're RECEIVING from?
+    - Is github SENDING webhooks to the same https://smee.io URL you"re RECEIVING from?
 
     """
     webhook = ObjectifyJSON(request.json)
-    log.info(
-        f'Incoming webhook [{webhook.action}]: {json.dumps(webhook,  sort_keys=True, indent=4)}')
+    event = request.headers['X-Github-Event']
+    action = str(webhook.action).lower()
 
-    # Let's react only when a new Pull Requests has been opened.
-    if request.headers['X-Github-Event'] == 'pull_request' and str(webhook.action).lower() == 'opened':
-        # This webhooks has this schema - https://developer.github.com/v3/activity/events/types/#pullrequestevent
-        add_pr_comment(webhook)
+    if event == "check_suite" and action == "requested":
+        log.info("Check suite requested.")
+        check_suite_request_handler(webhook)
+    elif event == "check_run" and action == "created":
+        log.info(f"Check run {webhook.check_run.name} create confirmed.")
     else:
-        log.info("Irrelavant webhook.")
+        log.info(f"Ignore webhook event {event} action {action}")
 
-    return 'GOOD'
+    return "GOOD"
 
 
-if __name__ == 'app' or __name__ == '__main__':
+if __name__ == "app" or __name__ == "__main__":
     print(
-        f'\n\033[96m\033[1m--- STARTING THE APP: [{datetime.datetime.now().strftime("%m/%d, %H:%M:%S")}] ---\033[0m \n')
+        f"\n\033[96m\033[1m--- STARTING THE APP: [{datetime.datetime.now().strftime('%m/%d, %H:%M:%S')}] ---\033[0m \n")
     validate_env_variables()
     app.run()
--- gh_utils.py
+++ gh_utils.py
@@ -1,17 +1,21 @@
 import json
 import logging
+import threading
+from typing import Any, Dict, Tuple
+
 import requests
-import sys
 
-from requests.auth import HTTPBasicAuth
+from objectify_json import ObjectifyJSON
+from time import sleep
 
-# from gh_oauth_token import retrieve_token
-from bot_config import API_BASE_URL, GH_USER_TOKEN, GH_USER
+from constances import validations
+from gh_oauth_token import retrieve_token
+from bot_config import API_BASE_URL
 
 log = logging.getLogger(__name__)
 
 
-def make_github_rest_api_call(api_path, method='GET', params=None):
+def make_github_rest_api_call(url: str = None, api_path: str = None, method: str = "GET", params: Dict[str, Any] = None):
     """Send API call to Github using a personal token.
 
 Use this function to make API calls to the GitHub REST api
@@ -21,49 +25,119 @@ For example:
 `GET` the current user
 ---
 ```py
-me = make_github_rest_api_call('login')
+me = make_github_rest_api_call("login")
 ```
 
 `POST` to create a comment on a PR
 ---
 ```py
 new_comment = make_github_rest_api_call(
-    'repos/my_org/my_repo/issues/31/comments',
-    'POST', {
-        'body': "Hello there, thanks for creating a new Pull Request!"
+    "repos/my_org/my_repo/issues/31/comments",
+    "POST", {
+        "body": "Hello there, thanks for creating a new Pull Request!"
     }
 )
 ```
     """
 
-    # token = retrieve_token()
-    token = GH_USER_TOKEN
+    token = retrieve_token()
 
     # Required headers.
-    headers = {'Accept': 'application/vnd.github.antiope-preview+json',
-               'Content-Type': 'application/json',
-               #    'Authorization': f'Bearer {token}'
+    headers = {"Accept": "application/vnd.github.antiope-preview+json",
+               "Content-Type": "application/json",
+               "Authorization": f"Bearer {token}"
                }
 
     # API url
-    url = f'{API_BASE_URL}/{api_path}'
+    if not url:
+        url = f"{API_BASE_URL}/{api_path}"
+
     log.info(
-        f'sending {method.upper()} request to {url} w/ data {json.dumps(params)}')
+        f"sending {method.upper()} request to {url} w/ data {json.dumps(params)}")
     try:
-        if method.upper() == 'POST':
+        if method.upper() == "POST":
             response = requests.post(
                 url,
                 headers=headers,
                 data=json.dumps(params),
-                auth=HTTPBasicAuth(GH_USER, GH_USER_TOKEN)  # basic auth
             )
-        elif method.upper() == 'GET':
+        elif method.upper() == "GET":
             response = requests.get(
                 url,
                 headers=headers,
-                auth=HTTPBasicAuth(GH_USER, GH_USER_TOKEN)  # basic auth
             )
         else:
-            raise Exception('Invalid Request Method.')
+            raise Exception("Invalid Request Method.")
     except:
         log.exception("Could not make a successful API call to GitHub.")
+
+
+class CheckSuite:
+    def __init__(self, webhook: ObjectifyJSON):
+        self.webhook = webhook
+
+        # Note: in the real implementation these threads will be done through spawning jobs through task API.
+        start_check_runs_thread = threading.Thread(target=self.start_check_runs())
+        start_check_runs_thread.start()
+
+    def start_check_runs(self):
+        for test in validations:
+            create_check_run_thread = threading.Thread(target=self.create_check_run, args=(test["name"], ))
+            create_check_run_thread.start()
+            sleep(1)  # simulate spawning job delay
+
+    def create_check_run(self, name: str) -> None:
+        CheckRun(self.webhook, name)
+
+
+class CheckRun:
+    def __init__(self, webhook: ObjectifyJSON, name: str):
+        self.name: str = name
+
+        self.repo_full_name: str = str(webhook.repository.full_name)
+        self.check_suite_id: int = int(str(webhook.check_suite.id))
+        self.check_suite_url: str = str(webhook.repository.url) + "/check-runs"
+        self.head_branch: str = str(webhook.check_suite.head_branch)
+        self.head_sha: str = str(webhook.check_suite.head_sha)
+        self.git_url: str = str(webhook.repository.git_url)
+
+        log.info(f"Running tests for {self.repo_full_name} branch {self.head_branch} ({self.head_sha})")
+
+        self.post_check_run(check_status="in_progress")
+        self.process_check_run()
+
+    def post_check_run(self,
+                       check_status: str,
+                       check_conclusion: str = None,
+                       output_title: str = None,
+                       output_summary: str = None) -> None:
+
+        payload = {
+            "name": self.name,
+            "head_sha": self.head_sha,
+            "status": check_status,
+        }
+
+        if check_conclusion:
+            payload['conclusion'] = check_conclusion
+
+        if output_title and output_summary:
+            payload['output'] = dict(title=output_title, summary=output_summary)
+
+        make_github_rest_api_call(url=self.check_suite_url, method='POST', params=payload)
+
+    def get_process_time(self) -> int:
+        for test in validations:
+            if self.name == test["name"]:
+                return test["estimate_time"]
+
+        log.warning(f"Can't find the test {self.name}'s estimate time.")
+        return 10
+
+    def get_mock_test_result(self) -> Tuple[str, str]:
+        return "completed", "success"
+
+    def process_check_run(self) -> None:
+        sleep(self.get_process_time())
+        status, conclusion = self.get_mock_test_result()
+        self.post_check_run(check_status=status, check_conclusion=conclusion)
